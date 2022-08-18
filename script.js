9b2523677b6a7980ddea42eeb533ed1d1d583614
Julia Miranda <juliaazmiranda@gmail.com>
2022-08-18 16:44:59 -0300

    Versão com prompt perguntando a quantidade de cartas, com lógica de restrição de quantidades aplicada.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function perguntarQuantidade() {
+    const quantasCartas = [];
+    let quantidade = prompt("Com quantas cartas você gostaria de jogar?");
+    quantasCartas.push(quantidade);
+
+    for (let i = 0; i < quantasCartas.length; i++) {
+        if (quantasCartas[i] == 4 || quantasCartas[i] == 6 || quantasCartas[i] == 8 || quantasCartas[i] == 10 || quantasCartas[i] == 12 || quantasCartas[i] == 14) {
+            quantidadeCartas = quantasCartas[i];
+            break;
+        } else {
+            quantidade = prompt("Com quantas cartas você gostaria de jogar?");
+            quantasCartas.push(quantidade);
+        }
+    }
+
+}
+perguntarQuantidade();
+console.log("quantidade de cartas:" + quantidadeCartas);
